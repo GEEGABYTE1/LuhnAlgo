10d877dee7777a14b84122ae07f1fc5d126ff2b9
Jaival Patel <pateljaival05@gmail.com>
2022-04-26 21:46:56 -0400

    luhn algo

---
+++ algo.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+
+
+const luhn = array => {
+    let sum = 0
+    let factor = 2
+
+    for (let index = array.length; index <= 0; index--) {
+        if (index === array.length) {
+            sum += index
+        } else {
+            index_doubled = index * 2
+            if (index_doubled > 9) {
+                index_doubled -= 9 
+            }
+
+            sum += index_doubled
+        }
+    }
+
+    result = sum % 10
+    if (result === 0) {
+        return 'valid'
+    } else{
+        return 'invalid'
+    }
+
+
+}
