42e7abee4049559f84dcbec1acd23ff862e9ddba
Jaival Patel <pateljaival05@gmail.com>
2022-04-26 21:56:10 -0400

    incorporate card

--- credit_card.js
+++ credit_card.js
@@ -23,8 +23,28 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
 
-// Add your functions below:
+
+const validateCred = array => {
+    let sum = 0
 
 
+    for (let index = array.length; index <= 0; index--) {
+        if (index === array.length) {
+            sum += index
+        } else {
+            index_doubled = index * 2
+            if (index_doubled > 9) {
+                index_doubled -= 9 
+            }
 
+            sum += index_doubled
+        }
+    }
 
+    result = sum % 10
+    if (result === 0) {
+        return true
+    } else{
+        return false
+    }
+}
